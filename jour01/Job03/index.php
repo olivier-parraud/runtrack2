7bb5a5fee61d79b6c82f1d4fae12edbf3300c4d7
olivier.parraud <olivier.parraud@laplateforme.io>
2025-09-16 10:46:52 +0200

    Tableau fini mais le reste je comprend pas

---
+++ jour01/Job03/index.php
@@ -0,0 +1,75 @@
+<?php
+
+
+// Integers PHP
+$a = 123; // nombre decimal
+var_dump($a);
+echo "<br>";
+ 
+$b = -123; // nombre négatif
+var_dump($b);
+echo "<br>";
+ 
+$c = 0x1A; // nombre hexadecimal
+var_dump($c);
+echo "<br>";
+ 
+$d = 0123; // nombre octal
+var_dump($d);
+echo "<br>";   
+
+
+
+// STRINGS PHP
+$a = 'Hello world!';
+echo $a;
+echo '<br>';
+ 
+$b = "Hello world!";
+echo $b;
+echo "<br>";
+ 
+$c = 'Attend moi ici, J\'arrive.';
+echo $c;
+echo "<br>";    
+
+
+// Nombres à virgule flottante PHP ou doubles
+
+$a = 1.234;
+var_dump($a);
+echo "<br>";
+ 
+$b = 10.2e3;
+var_dump($b);
+echo "<br>";
+ 
+$c = 4E-10;
+var_dump($c);
+
+
+// Booléens PHP
+
+// Assigner une valeur TRUE à une variable
+$show_error = true;
+var_dump($show_error);
+echo "<br>";
+echo "<br>";
+
+
+
+//TABLEAU PHP 
+
+echo "<table>
+
+<tr><th> Nom </th><th> Type </th><th> Valeur </th></tr>";
+
+echo "<tr><td> Or </td><td> +++ </td><td> 1000 </td></tr>";
+
+echo "<tr><td> Argent </td><td> ++ </td><td> 500 </td></tr>"; 
+
+echo "<tr><td> Cruivre </td><td> + </td><td> 10 </td></tr>";
+
+echo "</table>";
+
+?>
--- jour01/job02/index.php
+++ jour01/job02/index.php
@@ -3,10 +3,12 @@
 // en une ligne
 echo "Hello Ely"; 
 
+echo "<br>";
 
 // en un bloc
 echo "Hello";
+echo " ";
 echo "Ely";
-
+echo "!";
 
 ?> 
