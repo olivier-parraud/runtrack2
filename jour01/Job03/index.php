b88157548803bd533e15a1c1dced7087e8be61c7
olivier.parraud <olivier.parraud@laplateforme.io>
2025-09-16 11:06:20 +0200

    Job03 fini

--- jour01/Job03/index.php
+++ jour01/Job03/index.php
@@ -1,60 +1,12 @@
 <?php
-
-
-// Integers PHP
-$a = 123; // nombre decimal
-var_dump($a);
-echo "<br>";
- 
-$b = -123; // nombre négatif
-var_dump($b);
-echo "<br>";
- 
-$c = 0x1A; // nombre hexadecimal
-var_dump($c);
-echo "<br>";
- 
-$d = 0123; // nombre octal
-var_dump($d);
-echo "<br>";   
-
-
-
-// STRINGS PHP
-$a = 'Hello world!';
-echo $a;
-echo '<br>';
- 
-$b = "Hello world!";
-echo $b;
-echo "<br>";
- 
-$c = 'Attend moi ici, J\'arrive.';
-echo $c;
-echo "<br>";    
-
-
-// Nombres à virgule flottante PHP ou doubles
-
-$a = 1.234;
-var_dump($a);
-echo "<br>";
- 
-$b = 10.2e3;
-var_dump($b);
-echo "<br>";
- 
-$c = 4E-10;
-var_dump($c);
-
-
-// Booléens PHP
-
-// Assigner une valeur TRUE à une variable
-$show_error = true;
-var_dump($show_error);
-echo "<br>";
-echo "<br>";
+// booléen 
+$var1=false;
+// Entier
+$var2=7;
+// Nombre à virgule flottante
+$var3=7.2;
+// Chaîne de caractères
+$var4="je crée une chaîne de caractères";
 
 
 
@@ -73,3 +25,4 @@ echo "<tr><td> Cruivre </td><td> + </td><td> 10 </td></tr>";
 echo "</table>";
 
 ?>
+
