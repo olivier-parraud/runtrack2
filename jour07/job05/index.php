1b39ee101adc6780ddc035affe2c8801839dbdf8
olivier.parraud <olivier.parraud@laplateforme.io>
2025-09-24 11:24:50 +0200

    Jour 7
    Job05

---
+++ jour07/job05/index.php
@@ -0,0 +1,27 @@
+<?php
+
+
+function occurence($str,$char) {
+
+$count = 0;
+$i = 0;
+
+while (isset($str[$i])) {
+        if ($str[$i] === $char) {
+            $count++;
+        }
+        if ($str[$i] === "E") {
+            $count++;
+        }
+            
+        $i++;
+    }   
+
+    return $count;
+}
+
+$texte = "Bienvenue en enfer, suivez votre guide Ely";
+$caractères = "e";
+$resultat = occurence($texte,$caractères);
+
+echo "Le nombre de caractères . <b>$caractères</b> . apparait . <b>$resultat</b> . fois dans : <u>$texte.</u>";
